b41726beaa5eae1ffcd24b4391cdaa827a3ba4ef
Rohith Reddy GK <gkrohithreddy@gmail.com>
2025-09-30 18:57:09 +0530

    The output is little more formatted with some animations and a 3-color icons indicating, excellent or moderate or poor.

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -18,12 +18,30 @@ const Footer = () => {
           © {new Date().getFullYear()} ADMET-AI Platform — All rights reserved.
         </p>
 
-        <div className="flex space-x-6 items-center text-5xl justify-center md:justify-start">
-          <FaReact className="text-cyan-500 dark:text-cyan-400 hover:scale-110 transition" title="React" />
-          <SiTailwindcss className="text-sky-400 dark:text-sky-400 hover:scale-110 transition" title="Tailwind CSS" />
-          <SiFlask className="text-black dark:text-white hover:scale-110 transition" title="Flask" />
-          <MdScience className="text-green-600 dark:text-green-400 hover:scale-110 transition" title="RDKit (Molecule Science)" />
-          <TbDatabaseCog className="text-purple-600 dark:text-purple-400 hover:scale-110 transition" title="TDC (Therapeutics Data Commons)" />
+        <div className="flex items-center space-x-20">
+          <div className="flex space-x-6 items-center text-5xl justify-center md:justify-start">
+            <FaReact className="text-cyan-500 dark:text-cyan-400 hover:scale-110 transition" title="React" />
+            <SiTailwindcss className="text-sky-400 dark:text-sky-400 hover:scale-110 transition" title="Tailwind CSS" />
+            <SiFlask className="text-black dark:text-white hover:scale-110 transition" title="Flask" />
+            <MdScience className="text-green-600 dark:text-green-400 hover:scale-110 transition" title="RDKit (Molecule Science)" />
+            <TbDatabaseCog className="text-purple-600 dark:text-purple-400 hover:scale-110 transition" title="TDC (Therapeutics Data Commons)" />
+          </div>
+
+          <div className="flex space-x-2 items-center">
+            <div className="flex flex-col items-center">
+              <span className="w-4 h-4 rounded-full bg-green-500 mb-2"></span>
+              <span className="w-4 h-4 rounded-full bg-yellow-500 mb-2"></span>
+              <span className="w-4 h-4 rounded-full bg-red-500 mb-2"></span>
+              <span className="w-4 h-4 rounded-full bg-gray-500"></span>
+            </div>
+
+            <div className="flex flex-col justify-between h-full">
+              <span>Excellent</span>
+              <span>Moderate</span>
+              <span>Poor</span>
+              <span>None</span>
+            </div>
+          </div>
         </div>
       </div>
     </motion.footer>
